905d523fc508dfd0365ca626ad21a9ea9f1c1c9e
Samuelhimika <96009947+Samuelhimika@users.noreply.github.com>
2022-10-20 20:22:44 +0300

    Add files via upload

---
+++ Eco/Index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+      <link rel="stylesheet" href=" stylesheet.css">
+      <title>Make Africa Great</title>
+      
+ <header>JOINING OUR EFFORTS AS THE YOUNG AFRICAN GENERATION
+       TO REGAIN 
+      OUR  DIGNITY AS THE AFRICAN RACE</header>";
+
+      <body>
+
+      <?php
+
+      echo "<hr>";
+
+      echo  "<h1> Mission</h1>";
+
+      echo "<p> Bringing up a generation that clearly understands and defines their role in decision making , the role of their local and national leaders
+         and  being accountable   and  transperant Citizens of impact in their respective . </p>";
+
+      echo "<hr>";
+       echo  "<h2> Aims & objectives</h1>";
+      echo "<p> 1.Mass involvement of the youth to particapate in desicision making  at  both levels</p>";
+      echo "<p> 2.Encouraging  mass participation towards being accountable and tasking for accountability and transperancy  in Gov't  processes in
+      regard to service delivery and its objects.</p>";
+      echo "<p>3. Formation of  associations that will help the genaral public to take track of  roles of the local leaders and participation.</p>";
+      echo "<p> 4.Sensitation of the general public about  the government laws , level of participation and their impact towards the 
+      deveolpment of our communities and the country at large. </p>";
+
+
+
+     
+     echo "<p> 1.Mass involvement of the youth to particapate in desicision making  at  both levels</p>";
+     echo "<p> 2.Encouraging  mass participation towards being accountable and tasking for accountability and transperancy  in Gov't  processes in
+     regard to service delivery and its objects.</p>";
+
+     echo "<img src='IMG-2723.jpg' </>";
+     echo "<p>3. Formation of  associations that will help the genaral public to take track of  roles of the local leaders and participation.</p>";
+     echo "<p> 4.Sensitation of the general public about  the government laws , level of participation and their impact towards the 
+     deveolpment of our communities and the country at large. 
+     Since the Arab Spring,7 many young people in different countries in Africa have demonstrated
+      against dictatorships, the extension of presidential term limits, the lack of transparency
+       during elections and unpopular socio-economic policies. In many of these protests, 
+       youth movements have managed to gain considerable success in removing
+        political leaders and effecting policy change, but they have failed to
+         bring systematic political, social and economic change because of a
+          lack of influence in post-protest politics and government.
+           However, in Sudan, following the fall of Omar al-Bashir 
+           
+           in April 2019, protests led to the formation of a coalition
+            government with the military, and a roadmap for a civilian government through elections.
+             Influencing post-protest politics and development has been difficult for many protest movements in Africa over the last decade. 
+             The Sudanese experience can be viewed as a step forward by a protest movement to influencing post-protest 
+             politics and development by achieving the will of the people and the primary objective of ending dictatorship and 
+             creating a new Sudan with a democratic 
+     government, this can also lead to political and socio-economic development.</p>";
+
+     echo '<p> 5. Young people have been on the forefront during demonstrations in Africa for decades. 
+     Since the Arab Spring, youth movements have been leading demonstrations across Africa against 
+     dictatorship in countries such as Tunisia, Egypt, Burkina Faso, Senegal and Sudan. 
+     These movements marked the end of the long-term leadership of presidents Zine El Abidine Ben Ali, Muhammad
+      Hosni El Sayed Mubarak, Blaise Compaoré, Abdoulaye Wade and Bashir respectively. 
+      In some countries, such as the Democratic Republic of the Congo (DRC) and Burundi, young people have 
+      demonstrated against proposals to make constitutional amendments to extend presidential term limits by 
+      leaders including Joseph Kabila in the DRC, Alpha Condé in Guinea, Pierre
+       Nkurunziza in Burundi and Compaoré in Burkina Faso. In South Africa, youth protested
+        against an increase in university fees under the #FeesMustFall movement </p>';
+
+        echo "<img src='Batwa.jpg' </>";
+
+
+        echo '<p> 6.   The growing upsurge of youth pr
+        otests, crystallised in the word “enough!”, must be understood in the context of this generation’s struggles for pol
+        itical, social and economic emancipation.8 Young people in Africa are the most affected by unemployment and 
+        underemployment and they are struggling to survive. They are poor and are stuck in “waithood” – 
+        defined by Honwana as a prolonged period of suspension in which people’s access to social adulthood is delayed or denied. 
+        Whatever their class or background, many youth cannot afford to form families and households and are unable 
+        to become fully independent and partake in the privileges and responsibilities of adult life. Apart from material 
+        deprivation, “waithood” is also marked by political marginalisation, a lack of voice and a loss of dignity.
+         Young people have attempted to escape this trap through other strategies, such as venturing into the informal 
+         economy, migrating to other countries or becoming involved in criminal activities. 
+         Some even join extremist groups such as Al-Shabaab and Boko Haram, among others, as a way to survive. 
+         Despite the existence of several platforms for participation and the expression of discontent 
+         – such as forming or joining political parties and participating in local elections, or forming civil society organisations – youth in Africa have resorted to demonstrations as an effective way of expressing their views on various issues. This is likely because other platforms are not as effective as protests – they do not produce instant results, or the youth are manipulated or captured. This may explain why young people, regardless of their numbers, in many cases do not participate in processes such as elections, either as candidates or voters. They feel marginalised and unrepresented and have suffered the most from the outcomes of elections without any considerable change to their life. It is very difficult for many youth in Africa to join political parties and influence systems of power, because they lack the capacity that is usually determined by wealth and status. The structural barriers in political parties are also not easy to overcome. In addition, voting may have lost its perceived value as a democratic right among young Africans due to a record of lack of credibility with the electoral process. This has often resulted in both pre- and post-election violence. Ikomu is of the view that attending protests, joining a civil society campaign or using social media to share an opinion on a particular policy tend to be more inclusive for the youth, as these are more horizontal, more transparent and more invested in identifying specific problems affecting the demographic than traditional political channels. She is also of the view that political parties are highly hierarchal and structural, making it hard for young people to fully take advantage of this institution. Challenges such as the high cost of party politics, the preference of party loyalists over competent individuals and limited technical expertise among youth limit participation and dampen interests, 
+         even where political parties have youth wings</p>';
+
+echo'<footer> Samuel Hiimika45@ gmail.com</footer>'
+      ?>
+
+
+</body>
+</html>
